b6b0c3e876b7b2d09bb67f6472fb1234b44734ac
fosterg4 <dikkyhardian@gmail.com>
2020-10-04 06:42:12 +0700

    ntp

--- ntp.sh
+++ ntp.sh
@@ -1,8 +1,16 @@
-#add server time /etc/ntp.conf
+# untuk install ntp
+yum install ntp
+
+
+#tambahkan server time ke /etc/ntp.conf lalu beri tag server default nya
 server 0.id.pool.ntp.org iburst
 server 1.id.pool.ntp.org iburst
 server 2.id.pool.ntp.org iburst
 server 3.id.pool.ntp.org iburst
+
+
+#start ntp
+systemctl start ntpd
 
 
 #update time by server
