12b027568b99f2cd949328eb3d2cbe9efc28ce06
fosterg4 <dikkyhardian@gmail.com>
2020-10-07 20:38:47 +0700

    delete space

--- setfirst.sh
+++ setfirst.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 #open message
 echo "$(tput setaf 1)untuk menghindari terjadinya error pastikan OS dalam keadaan fresh install$(tput sgr 0)$(tput sgr 0)"
 echo "$(tput setaf 1)Jalankan script ini dengan user root$(tput sgr 0)$(tput sgr 0)"
